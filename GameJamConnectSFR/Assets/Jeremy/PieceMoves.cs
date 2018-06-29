971a0c11edc0d083c8f9eb77c153caf1302e23b8
Jeremykaito <jh152891@edu.ece.fr>
2018-06-30 01:57:33 +0200

    Pièce Tetris
    Déplacement de pièce
    Physique de glisse
    Assets temporaires

---
+++ GameJamConnectSFR/Assets/Jeremy/PieceMoves.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMoves : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D body;
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+    }
+
+    public void HandleMovement(float horizontal)
+    {
+        body.velocity = new Vector2(horizontal * moveSpeed, body.velocity.y);
+    }
+}
