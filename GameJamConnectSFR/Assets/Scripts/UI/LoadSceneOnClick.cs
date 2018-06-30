ab848ba70cdda80ec1dd5b01b542e727cc834b92
n.fontes <n.fontes@student.isartdigital.com>
2018-06-30 02:41:57 +0200

    Add Behaviour tree and load scene

---
+++ GameJamConnectSFR/Assets/Scripts/UI/LoadSceneOnClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    public class LoadSceneOnClick : MonoBehaviour
+    {
+        public int SceneIndex;
+        public LoadSceneMode LoadSceneMode;
+        public bool AsyncLoad;
+
+        public void LoadScene()
+        {
+            if (AsyncLoad)
+            {
+                SceneManager.LoadSceneAsync(SceneIndex, LoadSceneMode);
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneIndex, LoadSceneMode);
+            }
+        }
+    }
+}
